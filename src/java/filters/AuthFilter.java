571cab54983bd6162293e67a598d61816ab5e3ed
Ninjazx9r <90583370+Ninjazx9r@users.noreply.github.com>
2022-04-20 18:51:00 -0600

    Lab 10 Here at last

---
+++ src/java/filters/AuthFilter.java
@@ -0,0 +1,52 @@
+
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class AuthFilter implements Filter {
+
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpSession session = httpRequest.getSession();
+        String email = (String)session.getAttribute("email");
+        
+        if(email == null) {
+            HttpServletResponse httpResponse = (HttpServletResponse)response;
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        
+        
+        // Any code before chain.filter() will be executed before the servlet
+        chain.doFilter(request, response);
+        // Any code after chain.filter() will be executed after the servlet
+    }
+    
+    
+    // These methods will not be used , however they do need to exist
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+    @Override
+    public void destroy() {}    
+    
+    
+    
+    
+}
